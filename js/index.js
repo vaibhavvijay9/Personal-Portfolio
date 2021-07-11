7869e488d3e00bed6a4a259b9daee403fc00b931
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2021-07-11 11:46:46 +0530

    LightHouse performance changes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,21 @@
 let flag = 0;
 
 function setVisibility() {
-    if (flag == 0) {
-        $('.view-more').show();
-        $('#view-projects').text('VIEW LESS');
-        flag = 1;
-    } else {
-        $('.view-more').hide();
-        $('#view-projects').text('VIEW MORE');
-        flag = 0;
+    let viewMoreCards = document.getElementsByClassName('view-more');
+    let viewBtn = document.getElementById('view-projects');
+    if(viewMoreCards && viewMoreCards.length > 0) {
+        if (flag == 0) {
+            for (let i = 0; i < viewMoreCards.length; i++) {
+                viewMoreCards[i].style.display = 'block';
+            }
+            viewBtn.innerHTML = 'VIEW LESS';
+            flag = 1;
+        } else {
+            for (let i = 0; i < viewMoreCards.length; i++) {
+                viewMoreCards[i].style.display = 'none';
+            }
+            viewBtn.innerHTML = 'VIEW MORE';
+            flag = 0;
+        }
     }
 }
